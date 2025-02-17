e96afd0e65286eb8371a0f6ba666927f3da4c0b3
Gustavo Stinghen <gustavobr.developer@gmail.com>
2025-02-16 23:46:10 -0300

    Adição do React Router e Home
    Criação do componente Home para trabalhar com sistema de login usando o Meteor accounts. Porém a aplicação está a retornar um erro

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { ContactForm } from './ContactForm';
-import { ContactList } from './ContactList';
+import React from 'react';;
 import { Header } from './Header';
-import { Wallet } from './Wallet';
+import { BrowserRouter } from "react-router";
+import { Home } from './Home';
 
 
 export const App = () => (
 
-  <div>
-    <Header />
-    <div className="min-h-full">
-      <div className="max-w-4xl mx-auto p-2">
-        <Wallet/>
-        <ContactForm />
-        <ContactList />
+  <BrowserRouter>
+    <div>
+      <Header />
+      <div className="min-h-full">
+        <div className="max-w-4xl mx-auto p-2">
+          <Home/>
+        </div>
       </div>
     </div>
-  </div>
-
+  </BrowserRouter>
 );
